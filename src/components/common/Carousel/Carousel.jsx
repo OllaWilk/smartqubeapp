6f7371c48c2ab888ddb60bf2d533f446bd29f1e3
Aleksandra Wilk <alex.dev.wilk@gmail.com>
2023-12-15 13:15:11 +0100

    add carousel to solutions view

---
+++ src/components/common/Carousel/Carousel.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+import styles from "./Carousel.module.scss";
+
+export const Carousel = ({ carousel }) => {
+  const { header, span, text, button, linkTo, images } = carousel;
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToPrevious = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
+  };
+
+  const goToNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  return (
+    <>
+      <div className={styles.carouselContainer}>
+        <div className={styles.text}>
+          <h2>
+            {header}
+            <span>{span}</span>
+          </h2>
+          <p>{text}</p>
+          <a
+            href={linkTo}
+            className={styles.caltulatorBtn}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {button}
+          </a>
+        </div>
+        <div className={styles.slider}>
+          {images.map((image, index) => (
+            <div
+              key={`carousel-img-${index}-${image}`}
+              className={`${styles.slide} ${
+                index === currentIndex ? `${styles.active}` : ""
+              }`}
+            >
+              <img src={image} alt={`Slide ${index}`} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={styles.slideNavigation}>
+        <div></div>
+        <div className={styles.prevNext}>
+          <a
+            href="https://www.iceqube.com"
+            className={styles.iceQubeName}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ICEqube.com
+          </a>
+          <span className="prev" onClick={goToPrevious}>
+            &lt;
+          </span>
+          <span className="next" onClick={goToNext}>
+            &gt;
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+Carousel.propTypes = {
+  carousel: PropTypes.shape({
+    header: PropTypes.string,
+    span: PropTypes.string,
+    text: PropTypes.string,
+    button: PropTypes.string,
+    linkTo: PropTypes.string,
+    images: PropTypes.arrayOf(PropTypes.string),
+  }),
+};
--- src/components/views/Solutions/Solutions.jsx
+++ src/components/views/Solutions/Solutions.jsx
@@ -1,55 +1,17 @@
-import React from "react";
-import { GrFormNext, GrFormPrevious } from "react-icons/gr";
-
+import React, { useState } from "react";
+import { solutions } from "../../../db/en/solutions";
 import styles from "./Solutions.module.scss";
-import { icequbeProducts, icons } from "../../../images";
+import { Carousel } from "../../common";
 
 export const Solutions = () => {
+  const { carousel } = solutions;
   return (
-    <div className={styles.bannerContainer}>
-      <div className={`container`}>
-        <div className={styles.content}>
-          <div className={styles.text}>
-            <h2>
-              Design Your Air Conditioner with <span>IceQube.</span>
-            </h2>
-            <p>
-              ENCLOSURES FAN SYSTEMS, THERMOELECTRIC AIR CONDITIONERS , HEAT
-              EXCHANGERS, HAZARDOUS LOCATION AIR CONDITIONERS, HAZARDOUS
-              LOCATION HEAT EXCHANGERS, SERVER RACK/SHELTER CONTAINER COOLERS
-            </p>
-            <a
-              href="https://www.iceqube.com/technical/calculator/"
-              className={styles.caltulatorBtn}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Visit the website and calculate your project today.
-            </a>
-          </div>
-          <div className={styles.slider}>
-            <div className={`${styles.slides} active`}>
-              <img src={icequbeProducts.GroupPicture} alt="img" />
-            </div>
-          </div>
-        </div>
-
-        <div className={styles.slideNavigation}>
-          <div></div>
-          <div className={styles.prevNext}>
-            <a
-              href="https://www.iceqube.com"
-              className={styles.iceQubeName}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              ICEqube.com
-            </a>
-            <span className="prev">{/* <GrFormPrevious /> */}</span>
-            <span className="next">{/* <GrFormNext /> */}</span>
-          </div>
+    <>
+      <div className={styles.bannerContainer}>
+        <div className={`container`}>
+          <Carousel carousel={carousel} />
         </div>
       </div>
-    </div>
+    </>
   );
 };
