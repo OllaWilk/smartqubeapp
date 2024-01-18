9e3c15cea7e8bda2c8b67c1038040e6a28cbc646
Aleksandra Wilk <alex.dev.wilk@gmail.com>
2024-01-18 12:37:12 +0100

    install react-devvice-detect and use it in video elements. if its mobile dont play video

--- src/components/common/Video/Video.jsx
+++ src/components/common/Video/Video.jsx
@@ -1,19 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { isMobile } from "react-device-detect";
 import PropTypes from "prop-types";
 
 export const Video = ({ src }) => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    if (
-      /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(
-        userAgent
-      )
-    ) {
-      setIsMobile(true);
-    }
-  }, []);
   return (
     <>
       <video src={src} autoPlay={!isMobile} loop muted type="video/mp4" />
