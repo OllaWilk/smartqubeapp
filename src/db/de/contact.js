44f8de65eaf6fe368a08e19ec4eb5dac7cacb0b2
Aleksandra Wilk <alex.dev.wilk@gmail.com>
2024-03-05 13:03:35 +0100

    add consent checkboxes to contact form

--- src/db/de/contact.js
+++ src/db/de/contact.js
@@ -47,14 +47,23 @@ export const contact = {
     },
   ],
   contactForm: {
-    name: "Geben Sie Ihren Namen ein",
-    companyName: "Geben Sie den Namen Ihres Unternehmens ein",
-    email: "Geben Sie Ihre gültige E-Mail-Adresse ein",
+    name: "Geben Sie Ihren Namen ein *",
+    companyName: "Geben Sie den Namen Ihres Unternehmens ein *",
+    email: "Geben Sie Ihre gültige E-Mail-Adresse ein *",
     position: "Geben Sie Ihre Berufsposition ein",
-    phoneNumber: "Geben Sie Ihre Telefonnummer ein",
-    message: "Geben Sie Ihre Nachricht ein",
+    phoneNumber: "Geben Sie Ihre Telefonnummer ein *",
+    message: "Geben Sie Ihre Nachricht ein *",
     requiredFields:
       "Bitte beachten Sie: Alle mit einem Sternchen (*) markierten Felder sind erforderlich.",
+    submit: "absenden",
+    isOver18: "Ich erkläre, dass ich über 18 Jahre alt bin; *",
+    dataProcessingConsent:
+      "Ich willige in die Verarbeitung meiner personenbezogenen Daten gemäß den in meinem Wohnort und dem Betriebsort von SmartQube geltenden Gesetzen ein, mit Sitz in Breslau, ul. Rakietowa 29E, 54-615 Wrocław KRS: 0000984616, NIP: 8971909651; REGON: 522685620, Vorschriften über die elektronische Dienstleistungserbringung sowie Datenschutzvorschriften zum Zweck der Übermittlung und zukünftigen Zusendung von kommerziellen Informationen. Die Bereitstellung personenbezogener Daten ist freiwillig, aber für die ordnungsgemäße Erbringung bestellter Dienstleistungen notwendig. Mir wurde mitgeteilt, dass ich das Recht auf Zugang zu meinen Daten, die Möglichkeit ihrer Berichtigung und die Anforderung, ihre Verarbeitung einzustellen habe – wobei das Einstellen der Verarbeitung personenbezogener Daten die Einstellung der Dienstleistungserbringung zur Folge hat. Der Datenverwalter ist SmartQube mit Sitz in Breslau, ul. Rakietowa 29E, 54-615 Wrocław KRS: 0000984616, NIP: 8971909651; REGON: 522685620 *",
+    GDPR: "Ich willige in die Verarbeitung meiner personenbezogenen Daten gemäß den Datenschutzvorschriften ein (insbesondere gemäß der Verordnung (EU) 2016/679 des Europäischen Parlaments und des Rates vom 27. April 2016 zum Schutz natürlicher Personen bei der Verarbeitung personenbezogener Daten, zum freien Datenverkehr und zur Aufhebung der Richtlinie 95/46/EG, sowie dem Gesetz vom 10. Mai 2018 zum Schutz personenbezogener Daten) im Zusammenhang mit der Sendung einer Anfrage über das Kontaktformular. Die Bereitstellung personenbezogener Daten ist freiwillig, aber notwendig für die Bearbeitung der Anfrage. Mir wurde mitgeteilt, dass ich das Recht auf Zugang zu meinen Daten, die Möglichkeit ihrer Berichtigung und die Anforderung, ihre Verarbeitung einzustellen habe. Der Datenverwalter ist SmartQube mit Sitz in Breslau, ul. Rakietowa 29E, 54-615 Wrocław KRS: 0000984616, NIP: 8971909651; REGON: 522685620 Vollständige",
+    gdrLink: [
+      "Informationen zur Verarbeitung personenbezogener Daten hier.",
+      "http://localhost:3000/privacy",
+    ],
   },
   errorsMessages: {
     name: "Bitte geben Sie Ihren Namen ein",
