4ee8124c79ed2699a503b0e705e7ab912b389ec0
OllaWilk <aleksandra.wilk.letkiewicz@gmail.com>
2024-03-05 18:23:25 +0100

    Add toggle functionality to display additional consent information

--- src/db/de/contact.js
+++ src/db/de/contact.js
@@ -56,10 +56,18 @@ export const contact = {
     requiredFields:
       "Bitte beachten Sie: Alle mit einem Sternchen (*) markierten Felder sind erforderlich.",
     submit: "absenden",
-    isOver18: "Ich erkläre, dass ich über 18 Jahre alt bin; *",
-    dataProcessingConsent:
+    isOver18: "Ich erkläre, dass ich über 18 Jahre alt bin *",
+    dataProcessingConsent: [
+      "Ich stimme der Verarbeitung meiner personenbezogenen Daten durch SmartQube mit Sitz in Breslau zu, um Dienstleistungen zu erhalten und kommerzielle Informationen zu bekommen ",
+      "Weitere Informationen",
       "Ich willige in die Verarbeitung meiner personenbezogenen Daten gemäß den in meinem Wohnort und dem Betriebsort von SmartQube geltenden Gesetzen ein, mit Sitz in Breslau, ul. Rakietowa 29E, 54-615 Wrocław KRS: 0000984616, NIP: 8971909651; REGON: 522685620, Vorschriften über die elektronische Dienstleistungserbringung sowie Datenschutzvorschriften zum Zweck der Übermittlung und zukünftigen Zusendung von kommerziellen Informationen. Die Bereitstellung personenbezogener Daten ist freiwillig, aber für die ordnungsgemäße Erbringung bestellter Dienstleistungen notwendig. Mir wurde mitgeteilt, dass ich das Recht auf Zugang zu meinen Daten, die Möglichkeit ihrer Berichtigung und die Anforderung, ihre Verarbeitung einzustellen habe – wobei das Einstellen der Verarbeitung personenbezogener Daten die Einstellung der Dienstleistungserbringung zur Folge hat. Der Datenverwalter ist SmartQube mit Sitz in Breslau, ul. Rakietowa 29E, 54-615 Wrocław KRS: 0000984616, NIP: 8971909651; REGON: 522685620 *",
-    GDPR: "Ich willige in die Verarbeitung meiner personenbezogenen Daten gemäß den Datenschutzvorschriften ein (insbesondere gemäß der Verordnung (EU) 2016/679 des Europäischen Parlaments und des Rates vom 27. April 2016 zum Schutz natürlicher Personen bei der Verarbeitung personenbezogener Daten, zum freien Datenverkehr und zur Aufhebung der Richtlinie 95/46/EG, sowie dem Gesetz vom 10. Mai 2018 zum Schutz personenbezogener Daten) im Zusammenhang mit der Sendung einer Anfrage über das Kontaktformular. Die Bereitstellung personenbezogener Daten ist freiwillig, aber notwendig für die Bearbeitung der Anfrage. Mir wurde mitgeteilt, dass ich das Recht auf Zugang zu meinen Daten, die Möglichkeit ihrer Berichtigung und die Anforderung, ihre Verarbeitung einzustellen habe. Der Datenverwalter ist SmartQube mit Sitz in Breslau, ul. Rakietowa 29E, 54-615 Wrocław KRS: 0000984616, NIP: 8971909651; REGON: 522685620 Vollständige",
+    ],
+
+    GDPR: [
+      "Ich stimme der Verarbeitung meiner persönlichen Daten durch SmartQube gemäß der DSGVO für Zwecke im Zusammenhang mit dem Kontaktformular zu",
+      "Weitere Informationen",
+      "Ich willige in die Verarbeitung meiner personenbezogenen Daten gemäß den Datenschutzvorschriften ein (insbesondere gemäß der Verordnung (EU) 2016/679 des Europäischen Parlaments und des Rates vom 27. April 2016 zum Schutz natürlicher Personen bei der Verarbeitung personenbezogener Daten, zum freien Datenverkehr und zur Aufhebung der Richtlinie 95/46/EG, sowie dem Gesetz vom 10. Mai 2018 zum Schutz personenbezogener Daten) im Zusammenhang mit der Sendung einer Anfrage über das Kontaktformular. Die Bereitstellung personenbezogener Daten ist freiwillig, aber notwendig für die Bearbeitung der Anfrage. Mir wurde mitgeteilt, dass ich das Recht auf Zugang zu meinen Daten, die Möglichkeit ihrer Berichtigung und die Anforderung, ihre Verarbeitung einzustellen habe. Der Datenverwalter ist SmartQube mit Sitz in Breslau, ul. Rakietowa 29E, 54-615 Wrocław KRS: 0000984616, NIP: 8971909651; REGON: 522685620 Vollständige",
+    ],
     gdrLink: [
       "Informationen zur Verarbeitung personenbezogener Daten hier.",
       "http://localhost:3000/privacy",
@@ -71,5 +79,9 @@ export const contact = {
     companyName: "Bitte geben Sie den Namen Ihres Unternehmens ein",
     email: "Bitte geben Sie eine gültige E-Mail-Adresse ein",
     message: "Bitte geben Sie Ihre Nachricht ein",
+    isOver18: "Sie müssen über 18 Jahre alt sein",
+    dataProcessingConsent: "Sie müssen der Datenverarbeitung zustimmen",
+    euLawConsent:
+      "Die Zustimmung zur Datenverarbeitung gemäß DSGVO ist erforderlich",
   },
 };
--- src/db/pl/contact.js
+++ src/db/pl/contact.js
@@ -57,10 +57,19 @@ export const contact = {
     requiredFields:
       "Proszę zwrócić uwagę: Wszystkie pola oznaczone gwiazdką (*) są wymagane.",
     submit: "wyślij",
-    isOver18: "Oświadczam, że mam więcej niż 18 lat; *",
-    dataProcessingConsent:
-      "Wyrażam zgodę na przetwarzanie moich danych osobowych zgodnie z obowiązującymi w miejscu mojego zamieszkania oraz miejscu prowadzenia przez SmartQube z siedzibą we Wrocławiu ul. Rakietowa 29E, 54-615 Wrocław KRS: 0000984616, NIP: 8971909651; REGON: 522685620 działalności przepisami dotyczącymi świadczenia usług drogą elektroniczną oraz przepisami dotyczącymi ochrony danych osobowych w celu przekazania i wysyłania w przyszłości informacji handlowej. Podanie danych osobowych jest świadome dobrowolne. Jest jednak niezbędne do prawidłowego świadczenia zamówionych usług. Zostałem poinformowany że przysługuje mi ich prawdo dostępu do swoich danych, możliwości ich poprawiania, żądania zaprzestania ich przetwarzania – z tym, że zaprzestanie przetwarzania danych osobowych wiązać się będzie z zaprzestaniem świadczenia usług. Administratorem danych jest SmartQube z siedzibą we Wrocławiu ul. Rakietowa 29E, 54-615 Wrocław KRS: 0000984616, NIP: 8971909651; REGON: 522685620 *",
-    GDPR: "Wyrażam zgodę na przetwarzanie moich danych osobowych zgodnie z obowiązującym przepisami dotyczącymi ochrony danych osobowych (w tym w szczególności rozporządzeniem Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE oraz ustawą z dnia 10 maja 2018 roku o ochronie danych osobowych) w związku z wysłaniem zapytania poprzez formularz kontaktowy. Podanie danych osobowych jest dobrowolne, ale niezbędne do przetworzenia zapytania. Zostałem poinformowany, że przysługuje mi ich prawdo dostępu do swoich danych, możliwości ich poprawiania, żądania zaprzestania ich przetwarzania. Administratorem danych jest SmartQube z siedzibą we Wrocławiu ul. Rakietowa 29E, 54-615 Wrocław KRS: 0000984616, NIP: 8971909651; REGON: 522685620",
+    isOver18: "Oświadczam, że mam więcej niż 18 lat *",
+    dataProcessingConsent: [
+      "Zgadzam się na przetwarzanie moich danych osobowych przez SmartQube z siedzibą we Wrocławiu w celu świadczenia usług i otrzymywania informacji handlowych.",
+      "Więcej informacji",
+      "Wyrażam zgodę na przetwarzanie moich danych osobowych zgodnie z obowiązującymi w miejscu mojego zamieszkania oraz miejscu prowadzenia przez SmartQube z siedzibą we Wrocławiu ul. Rakietowa 29E, 54-615 Wrocław KRS: 0000984616, NIP: 8971909651; REGON: 522685620 działalności przepisami dotyczącymi świadczenia usług drogą elektroniczną oraz przepisami dotyczącymi ochrony danych osobowych w celu przekazania i wysyłania w przyszłości informacji handlowej. Podanie danych osobowych jest świadome dobrowolne. Jest jednak niezbędne do prawidłowego świadczenia zamówionych usług. Zostałem poinformowany że przysługuje mi ich prawdo dostępu do swoich danych, możliwości ich poprawiania, żądania zaprzestania ich przetwarzania - z tym, że zaprzestanie przetwarzania danych osobowych wiązać się będzie z zaprzestaniem świadczenia usług. Administratorem danych jest SmartQube z siedzibą we Wrocławiu ul. Rakietowa 29E, 54-615 Wrocław KRS: 0000984616, NIP: 8971909651; REGON: 522685620",
+    ],
+
+    GDPR: [
+      "Zgadzam się na przetwarzanie moich danych osobowych przez SmartQube zgodnie z RODO dla celów związanych z formularzem kontaktowym.",
+      "Więcej informacji",
+
+      "Wyrażam zgodę na przetwarzanie moich danych osobowych zgodnie z obowiązującym przepisami dotyczącymi ochrony danych osobowych (w tym w szczególności rozporządzeniem Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE oraz ustawą z dnia 10 maja 2018 roku o ochronie danych osobowych) w związku z wysłaniem zapytania poprzez formularz kontaktowy. Podanie danych osobowych jest dobrowolne, ale niezbędne do przetworzenia zapytania. Zostałem poinformowany, że przysługuje mi ich prawdo dostępu do swoich danych, możliwości ich poprawiania, żądania zaprzestania ich przetwarzania. Administratorem danych jest SmartQube z siedzibą we Wrocławiu ul. Rakietowa 29E, 54-615 Wrocław KRS: 0000984616, NIP: 8971909651; REGON: 522685620",
+    ],
     gdrLink: [
       "Pełna treść informacji dotyczących przetwarzania danych osobowych jest tutaj. ",
       "http://localhost:3000/privacy",
@@ -72,5 +81,8 @@ export const contact = {
     companyName: "Proszę wpisać nazwę swojej firmy",
     email: "Proszę wpisać ważny adres e-mail",
     message: "Proszę wpisać swoją wiadomość",
+    isOver18: "Musisz mieć skończone 18 lat",
+    dataProcessingConsent: "Musisz wyrazić zgodę na przetwarzanie danych",
+    euLawConsent: "Wymagana jest zgoda na przetwarzanie danych zgodnie z RODO",
   },
 };
